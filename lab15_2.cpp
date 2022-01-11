b33062856533d2cf5028cc50d52b2049552876d5
FrienDDDD <friendjunk@gmail.com>
2022-01-11 17:56:21 +0700

    lab15_2.cpp

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 
-const int N = 5;
+const int N = 5 ;
 
 void inputMatrix(double [][N]);
+
 
 void findLocalMax(const double [][N], bool [][N]);
 
@@ -19,3 +20,50 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+
+
+	for(int j = 0;j < N;j++){
+		cout << "Row " << j+1 << ": "  ;
+		for(int i = 0;i < N;i++){
+			cin >> A[j][i] ;
+		}
+	}
+
+}
+
+void showMatrix(const bool A[][N]){
+
+	for(int j = 0;j < N;j++){
+		for(int i = 0;i < N;i++){
+			cout << A[j][i] << " " ;
+
+		}
+	cout << "\n" ;
+	}
+
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+
+	
+	for(int j = 0;j < N ;j++){
+		for(int i = 0;i < N; i++){
+			if((j != 0 && j != N-1 ) && (i != 0 && i != N-1 )){
+				if((A[j][i] >= A[j-1][i]) && (A[j][i] >= A[j+1][i]) && (A[j][i] >= A[j][i-1]) && (A[j][i] >= A[j][i+1])){
+					B[j][i] = true ;
+				}else{
+					B[j][i] = false ;
+				}
+			}else{
+				B[j][i] = false ;
+			}
+
+		}
+
+	}
+		
+}
+
+
